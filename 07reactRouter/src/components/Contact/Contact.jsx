fc4109b3a3e81bc9ad8cc3f8459882f2de93b1ea
Suraj Gupta <guptasuraj4455@gmail.com>
2024-12-07 19:59:16 +0530

    some change on submit button

--- 07reactRouter/src/components/Contact/Contact.jsx
+++ 07reactRouter/src/components/Contact/Contact.jsx
@@ -128,7 +128,7 @@ export default function Contact() {
                           </div>
 
                           <button
-                              type="submit"
+                              type="submit" onClick={alert("Submitted")}
                               className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
                           >
                               Submit
